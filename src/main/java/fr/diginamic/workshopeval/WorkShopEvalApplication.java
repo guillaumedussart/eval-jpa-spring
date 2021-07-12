1b74f2b9fecc29dfd2e2a17223a00d6d4a372930
guillaume dussart <guillaume@localdev.local>
2021-07-12 10:08:08 +0200

    start eval

---
+++ src/main/java/fr/diginamic/workshopeval/WorkShopEvalApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.workshopeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopEvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkShopEvalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/diginamic/workshopeval/WorkShopEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.workshopeval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopEvalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
